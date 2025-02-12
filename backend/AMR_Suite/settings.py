17348a03f9ec850d1c87506518aff0e3d85c1d4b
Muthuraj-Vairamuthu <muthuraj22307@iiitd.ac.in>
2025-02-12 14:23:20 +0530

    Update Design Alignment Errors

--- backend/AMR_Suite/settings.py
+++ backend/AMR_Suite/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+
+
+
 # Import settings from oauth_settings.py
 try:
     from .oauth_settings import (
@@ -25,7 +28,6 @@ except ImportError:
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
